c63bf491bee20d1b596e321a8067ebc2b451b46e
filiz-g <flzgl.1773@gmail.com>
2021-06-03 03:07:39 +0300

    ....

---
+++ src/oopIntro/ProductManager.java
@@ -0,0 +1,16 @@
+package oopIntro;
+
+public class ProductManager {
+	
+	
+	//ParametreliMetotlar
+	public void addToCart(Product product) {
+		
+	//System.out.println(product.name + " --> Sepete eklendi.");
+		
+		System.out.println("Sepete eklendi:"+ product.getName());
+	
+	
+
+}
+}
